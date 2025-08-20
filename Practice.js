1e94f77f1d0e24368f4b663065651cd68934e5f3
RagunathRa <ragunath065@gmail.com>
2025-08-20 09:42:16 +0530

    Day-10/loop

--- Practice.js
+++ Practice.js
@@ -114,3 +114,23 @@ function firstMissingPositive(nums) {
 console.log(firstMissingPositive([1, 2, 0]));          // 3
 console.log(firstMissingPositive([3, 4, -1, 1]));      // 2
 console.log(firstMissingPositive([7, 8, 9, 11, 12]));  // 1
+
+
+// find duplicate and remove÷
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+    var k = 1; // Start with the first element
+    for (var i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            console.log(nums[i], "----------lokesh", nums[k] ,"nums[i - 1]", nums[i - 1])
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+};
+
+var nums = [0,0,1,1,1,2,2,3,3,4];
+var newLength = removeDuplicates(nums);
+console.log(newLength); // Output: 2
+console.log(nums.slice(0, newLength)); // Output: [1, 2]
