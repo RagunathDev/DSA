e425c8292adc3a5b02692b3883b0cfcfde37e72d
RagunathRa <ragunath065@gmail.com>
2025-08-31 23:43:47 +0530

    Aug31-progess

--- check.js
+++ check.js
@@ -1,7 +1,24 @@
 
-function findmin(q, g) {
-   console.log("   q.concat(g)",   q.concat(g));
+// function findmin(q, g) {
+//    console.log("   q.concat(g)",   q.concat(g));
   
+// }
+// var q = [1,2] ,g = [3,4]
+// findmin(q, g)
+
+function newFun(x) {
+   for (let i = 0; i < x.length; i++) {
+      // console.log(x[i],"----console.log(x[i]);");
+     
+      for (let j = 0; j < x.length; j++) {
+         // const element = x[j];
+         // console.log(element, x[i]);
+         
+         if( x[i] !==  x[j]){
+           console.log(x[i] ,"o0o", x[j]);
+      }
+      }
+   }
 }
-var q = [1,2] ,g = [3,4]
-findmin(q, g)
+var a = [4,2,1,1,2];
+newFun(a);
