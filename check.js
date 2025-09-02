4abb105c7e8d57348004c970c1129db2bd6d7c7e
RagunathRa <ragunath065@gmail.com>
2025-09-02 22:32:56 +0530

    Array | 10:32 pm - 2/9/25 | Day17

--- check.js
+++ check.js
@@ -25,15 +25,45 @@
 //    }
 // }
 
-// on this function is used to find a duplicates in one number
-function newFun(x) {
-   let result = 0;
-   for (let i = 0; i < x.length; i++) {
-      result ^= x[i]; // XOR cancels out duplicates
-   }
-   return result;
+// // on this function is used to find a duplicates in one number
+// function newFun(x) {
+//    let result = 0;
+//    for (let i = 0; i < x.length; i++) {
+//       console.log(x[i]);
+      
+//       result ^= x[i]; // XOR cancels out duplicates
+//    }
+//    return result;
+// }
+// var a = [4,1,2,1,2];
+// var b = [0,1,0,1,0,1,99]
+
+// console.log(newFun(b));
+
+// find the unique number use liner complexity and 0(n) space
+
+function singleNumber(nums) {
+  let ones = 0, twos = 0;
+
+  for (let num of nums) {
+   console.log(num,"lk");
+   
+    ones = (ones ^ num) & ~twos;
+    console.log(ones,"---ones");
+    
+    twos = (twos ^ num) & ~ones;
+    console.log("twos", twos);
+
+    console.log("ones======++", ones);
+    console.log("----------------");
+    
+  }
+
+
+  return ones;
 }
-var a = [4,1,2,1,2];
-// var b = [2,3,2,3,4,4,5] 
-// newFun(a);
-console.log(newFun(a));
+// var xc = [2,2,3,2];
+var h = [0,1,0,1,0,8,7,8,7,1,99];
+
+console.log(singleNumber(h));
+// console.log(singleNumber(xc));
